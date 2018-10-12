0990003be592bb0cf348b4b90a71038e2066eb8a
alecoexposito <alecoexposito@gmail.com>
2018-10-12 10:55:06 -0400

    tests to get info from gps

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 var config = {};
-config.serverAllIp = '187.162.125.161'; // in case everything is in the same server
+config.serverAllIp = '192.168.1.100'; // in case everything is in the same server
 config.apiUrl = 'http://187.162.125.161:3007/api/v1';
 config.webSocketIp = '187.162.125.161';
 config.webSocketPort = 3000;
 
-config.bbPort = 3002
+config.bbPort = 3002;
 
 
 module.exports = config;
