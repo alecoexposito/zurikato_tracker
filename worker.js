091abf6b9d9792610a0bce9820a7d62bceca9bb9
alecoexposito <alecoexposito@gmail.com>
2018-10-15 16:34:01 -0400

    adding mdvr module

--- worker.js
+++ worker.js
@@ -9,6 +9,50 @@ var cors = require('cors');
 var config = require('./config');
 class Worker extends SCWorker {
 
+    constructor() {
+        console.log("constructor of worker");
+        setInterval(function() {
+            var options = {
+                hostname: '187.162.125.161',
+                port: 8088,
+                path: '/StandardApiAction_login.action?account=admin&password=admin',
+                method: 'GET'
+            };
+
+            console.log("enviando mensaje por websocket ");
+            this.mdvrSend();
+
+            http.request(options, function(res) {
+                // console.log('STATUS: ' + res.statusCode);
+                // console.log('HEADERS: ' + JSON.stringify(res.headers));
+
+                res.setEncoding('utf8');
+                res.on('data', function(data) {
+                    var jsession = JSON.parse(data).jsession;
+                    if(jsession == undefined)
+                        return;
+                    console.log(jsession);
+                    var options2 = {
+                        hostname: '187.162.125.161',
+                        port: 8088,
+                        path: '/StandardApiAction_queryUserVehicle.action?jsession=' + jsession,
+                        method: 'GET'
+                    };
+                    http.request(options2, function(res2) {
+                        res2.setEncoding('utf8');
+                        res2.on('data', function (data2) {
+                            console.log("segunda peticion", data2);
+                        });
+                    }).end();
+                });
+                // res.on('data', function (chunk) {
+                //     console.log('BODY: ' + chunk);
+                // });
+            }).end();
+        }, 10000);
+
+    }
+
     run() {
         console.log('   >> Worker PID:', process.pid);
         var app = require('express')();
