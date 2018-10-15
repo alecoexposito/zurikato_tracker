51af12385df2efd696575dff7f6de4d3d2e7e7dd
alecoexposito <alecoexposito@gmail.com>
2018-10-15 16:43:40 -0400

    adding mdvr module

--- worker.js
+++ worker.js
@@ -7,6 +7,8 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var cors = require('cors');
 var config = require('./config');
+var http = require('http');
+
 class Worker extends SCWorker {
 
     constructor() {
