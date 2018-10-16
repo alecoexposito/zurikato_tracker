655d4d046dfffa78298cedd68499f835b467ddae
alecoexposito <alecoexposito@gmail.com>
2018-10-16 11:30:46 -0400

    adding mdvr module

--- config.js
+++ config.js
@@ -3,8 +3,11 @@ config.serverAllIp = '192.168.1.100'; // in case everything is in the same serve
 config.apiUrl = 'http://187.162.125.161:3007/api/v1';
 config.webSocketIp = '187.162.125.161';
 config.webSocketPort = 3000;
-
 config.bbPort = 3002;
+
+// configs for mdvr
+config.mdvrApiIp = '187.162.125.161';
+config.mdvrApiPort = 8088
 
 
 module.exports = config;
