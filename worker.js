d2aa0687e3d888fedc633d577ea2d0460bfc526c
alecoexposito <alecoexposito@gmail.com>
2018-10-16 14:50:55 -0400

    adding mdvr module

--- worker.js
+++ worker.js
@@ -15,42 +15,8 @@ class Worker extends SCWorker {
         super();
         var _this = this;
         console.log("constructor of worker");
-        setInterval(function() {
-            var options = {
-                hostname: config.mdvrApiIp,
-                port: config.mdvrApiPort,
-                path: '/StandardApiAction_login.action?account=admin&password=admin',
-                method: 'GET'
-            };
-
-
-            http.request(options, function(res) {
-                // console.log('STATUS: ' + res.statusCode);
-                // console.log('HEADERS: ' + JSON.stringify(res.headers));
-
-                res.setEncoding('utf8');
-                res.on('data', function(data) {
-                    var jsession = JSON.parse(data).jsession;
-                    if(jsession == undefined)
-                        return;
-                    console.log(jsession);
-                    var options2 = {
-                        hostname: config.mdvrApiIp,
-                        port: config.mdvrApiPort,
-                        path: '/StandardApiAction_queryUserVehicle.action?jsession=' + jsession,
-                        method: 'GET'
-                    };
-                    http.request(options2, function(res2) {
-                        res2.setEncoding('utf8');
-                        res2.on('data', function (data2) {
-                            console.log("segunda peticion", data2);
-                            _this.mdvrSend(_this.scServer, data2);
-                        });
-                    }).end();
-                });
-            }).end();
-        }, 10000);
-
+        var mdvrController = require(__dirname + '/lib/mdvrController')(this.scServer);
+        mdvrController.mdvrFirstSend();
     }
 
     run() {
