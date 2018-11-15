bdb6cc348119fc42aebc8a8347a225b99c3b9958
alecoexposito <alecoexposito@gmail.com>
2018-11-15 12:12:58 -0500

    adding alarms for mdvr using apis

--- config.js
+++ config.js
@@ -6,16 +6,16 @@ config.webSocketPort = 3000;
 config.bbPort = 3002;
 
 // configs for mdvr
-// config.mdvrApiIp = '69.64.51.66';
-// config.mdvrApiPort = 80;
-// config.mdvrApiUser = 'admin';
-// config.mdvrApiPass = 'dtvaxnet';
+config.mdvrApiIp = '69.64.51.66';
+config.mdvrApiPort = 80;
+config.mdvrApiUser = 'admin';
+config.mdvrApiPass = 'dtvaxnet';
 
 // configs for mdvr server de pruebas
-config.mdvrApiIp = '187.162.125.161';
-config.mdvrApiPort = 8080;
-config.mdvrApiUser = 'admin';
-config.mdvrApiPass = 'admin';
+// config.mdvrApiIp = '187.162.125.161';
+// config.mdvrApiPort = 8080;
+// config.mdvrApiUser = 'admin';
+// config.mdvrApiPass = 'admin';
 
 module.exports = config;
 
