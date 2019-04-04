d7f4643f38d7054082c827043469b69fde93bb1c
Alejandro <alecoexposito@gmail.com>
2019-04-04 12:42:37 -0400

    setting configs for USA server

--- config.js
+++ config.js
@@ -1,7 +1,14 @@
 var config = {};
-config.serverAllIp = '192.168.1.100'; // in case everything is in the same server
-config.apiUrl = 'http://187.162.125.161:3007/api/v1';
-config.webSocketIp = '187.162.125.161';
+// config.serverAllIp = '192.168.1.100'; // in case everything is in the same server
+// config.apiUrl = 'http://187.162.125.161:3007/api/v1';
+// config.webSocketIp = '187.162.125.161';
+// config.webSocketPort = 3000;
+// config.bbPort = 3002;
+
+// configs for server USA
+config.serverAllIp = '69.64.32.172'; // in case everything is in the same server
+config.apiUrl = 'http://69.64.32.172:3007/api/v1';
+config.webSocketIp = '69.64.32.172';
 config.webSocketPort = 3000;
 config.bbPort = 3002;
 
